ff7c0d9375e4c8b16b5bf27e562ce02c7ca86edc
Sergey Kanukov <s.kanukov@gmail.com>
2026-05-05 18:06:21 +0300

    Update down migrations

--- migrations/init_floww.down.sql
+++ migrations/init_floww.down.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS floww_activity_jobs;
+DROP TABLE IF EXISTS floww_activities;
 DROP TYPE IF EXISTS floww_activity_status;
 
-DROP TABLE IF EXISTS floww_workflow_jobs;
-DROP TYPE IF EXISTS floww_workflow_job_status;
+DROP TABLE IF EXISTS floww_workflow_tasks;
+DROP TYPE IF EXISTS floww_workflow_task_status;
 
 DROP TABLE IF EXISTS floww_workflows;
 DROP TYPE IF EXISTS floww_workflow_status;
